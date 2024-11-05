d87dad5f4f10eb83315512f58f23d8e6653c6313
Davi Gomes <daviszgomes@gmail.com>
2024-11-05 15:37:38 -0300

    Finish Main Source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
+function Tarefa({tarefa}) {
+  return <li>{tarefa}</li>;
+}
+
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+  const [novaTarefa, setNovaTarefa] = useState('');
+
+  const adicionarTarefa = () => {
+    if (novaTarefa.trim() !== '') {
+      setTarefas([...tarefas, novaTarefa]);
+      setNovaTarefa('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1> Lista de Tarefas </h1>
+      <input
+        type='text'
+        value={novaTarefa}
+        onChange={(e) => setNovaTarefa(e.target.value)}
+        placeholder='Digite uma tarefa'
+      />
+      <button onClick={adicionarTarefa}>Adicionar</button>
+
+      <ul>
+        {tarefas.map((tarefa, index) => (
+          <Tarefa key={index} tarefa={tarefa}/>
+        ))}
+      </ul>
     </div>
   );
 }
